55179a16f81034e263e9447a09cf3d36561853ab
MagmaDirt <103303927+MagmaDirt@users.noreply.github.com>
2023-03-03 12:59:46 -0700

    Update homework.asm

--- assembly/homework/homework.asm
+++ assembly/homework/homework.asm
@@ -10,6 +10,18 @@
 // PROBLEM 2: Counting Up From 1 to 10 in MIPS Assembly
 //==========================================================================================
 
+      li     $t0, 1
+
+Loop: move   $a0, $t0
+      li     $v0, 1
+      syscall
+      addi   $t0, $t0, 1
+    
+      bne    $t0, 11, Loop
+    
+      li     $v0, 10
+      syscall
+
 // You can develop your code via the MARS emulator and then paste it in here
 // Please use a jump in your solution
 // Please use sys-calls to output the value
